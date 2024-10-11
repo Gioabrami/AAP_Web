4f29bff27285115c778a0e290eca09d1a8df74a2
Giovanni <gioabrami@gmail.com>
2024-10-11 16:23:30 -0300

    Aap 7 pronta

--- components/script.js
+++ components/script.js
@@ -8,6 +8,20 @@ function loadPage(page) {
     })
     .then(data => {
         document.getElementById('content').innerHTML = data;
+
+        // Adicionar evento de clique após o conteúdo ser carregado
+        const buttons = document.querySelectorAll('.toggle-button');
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const targetId = button.getAttribute('data-target');
+                const targetDiv = document.getElementById(targetId);
+                if (targetDiv.classList.contains('hidden')) {
+                    targetDiv.classList.remove('hidden');
+                } else {
+                    targetDiv.classList.add('hidden');
+                }
+            });
+        });
     })
     .catch(error => {
         document.getElementById('content').innerHTML = `<p>${error.message}</p>`;
